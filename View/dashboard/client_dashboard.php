4dfe7784069c8d290c54c56f4bfdaa63d703b516
Aroham-Developer <36259829+Aroham-Developer@users.noreply.github.com>
2018-02-08 18:00:19 +0530

    dashboard

---
+++ View/dashboard/client_dashboard.php
@@ -0,0 +1,11 @@
+<div id="page-content" class="p20 clearfix">
+    
+    <div class="row">
+        <?php $this->load->view("clients/info_widgets"); ?>
+    </div>
+
+    <div class="">
+        <?php $this->load->view("clients/projects/index"); ?>
+    </div>
+</div>
+
